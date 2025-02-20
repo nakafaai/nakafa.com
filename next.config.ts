b126b317be61b6ac69d6f73a7d976ba118ee7435
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-02-20 19:30:44 +0100

    add million js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from 'next';
+import MillionLint from '@million/lint';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  // ...
 };
 
-export default nextConfig;
+export default MillionLint.next({ rsc: true })(nextConfig);
