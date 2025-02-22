d6d4af13d1393b1efb4d4944562a5d3e4b72e6e8
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-02-22 13:10:45 +0100

    generate metadata

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -15,6 +15,7 @@ import type { ReactNode } from "react";
 import "@/styles/globals.css";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import type { Metadata } from "next";
 
 type Props = {
   children: ReactNode;
@@ -23,13 +24,54 @@ type Props = {
 
 export async function generateMetadata({
   params,
-}: { params: Props["params"] }) {
+}: { params: Props["params"] }): Promise<Metadata> {
   const { locale } = await params;
   const t = await getTranslations({ locale, namespace: "Metadata" });
 
   return {
-    title: t("title"),
+    title: {
+      template: "%s - Nakafa",
+      default: t("title"),
+    },
     description: t("description"),
+    applicationName: "Nakafa",
+    creator: "Nabil Akbarazzima Fatih",
+    publisher: "PT. Nakafa Tekno Kreatif",
+    referrer: "origin-when-cross-origin",
+    metadataBase: new URL("https://nakafa.com"),
+    alternates: {
+      canonical: "https://nakafa.com",
+      languages: {
+        id: "https://nakafa.com/id",
+        en: "https://nakafa.com/en",
+      },
+    },
+    icons: {
+      icon: [{ url: "/logo.svg" }, new URL("/logo.svg", "https://nakafa.com")],
+      shortcut: [
+        { url: "/logo.svg" },
+        new URL("/logo.svg", "https://nakafa.com"),
+      ],
+      apple: [{ url: "/logo.svg" }, new URL("/logo.svg", "https://nakafa.com")],
+      other: [
+        { rel: "manifest", url: "/manifest.json" },
+        { rel: "apple-touch-icon", url: "/logo.svg" },
+      ],
+    },
+    robots: {
+      index: true,
+      follow: true,
+      nocache: false,
+      googleBot: {
+        index: true,
+        follow: true,
+        noimageindex: false,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+    category: "education",
   };
 }
 
