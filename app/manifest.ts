2ec20a479d757cc0b0a84e53768863c2d10e7cd1
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-02-22 23:38:13 +0100

    add manifest

---
+++ app/manifest.ts
@@ -0,0 +1,31 @@
+import type { MetadataRoute } from "next";
+import { getTranslations } from "next-intl/server";
+
+export default async function manifest(): Promise<MetadataRoute.Manifest> {
+  // Pick a locale that is representative of the app
+  const locale = "id";
+
+  const t = await getTranslations({
+    namespace: "Metadata",
+    locale,
+  });
+
+  return {
+    name: t("title"),
+    short_name: "Nakafa",
+    description: t("description"),
+    start_url: "/",
+    theme_color: "#f4f4f5",
+    background_color: "#f4f4f5",
+    display: "standalone",
+    orientation: "portrait",
+    scope: "/",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
+      },
+    ],
+  };
+}
