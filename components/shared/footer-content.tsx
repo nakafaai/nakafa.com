10ed1cc1b1d463d054e75cb8cbb4cfc8402b87bf
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-03-05 02:06:06 +0100

    add pagefind ignore

--- components/shared/footer-content.tsx
+++ components/shared/footer-content.tsx
@@ -9,7 +9,10 @@ type Props = {
 
 export function FooterContent({ children, className }: Props) {
   return (
-    <footer className={cn("relative mt-10 border-t pt-10 pb-20", className)}>
+    <footer
+      data-pagefind-ignore
+      className={cn("relative mt-10 border-t pt-10 pb-20", className)}
+    >
       <Particles className="pointer-events-none absolute inset-0 opacity-50" />
       <div className="z-10 mx-auto max-w-3xl px-4">{children}</div>
     </footer>
