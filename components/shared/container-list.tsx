276e693fddf44783868efa297d861c4351d26c15
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-03-08 05:48:31 +0100

    fix gap

--- components/shared/container-list.tsx
+++ components/shared/container-list.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export function ContainerList({ children, className }: Props) {
   return (
-    <div className={cn("grid grid-cols-1 gap-10 sm:grid-cols-2", className)}>
+    <div className={cn("grid grid-cols-1 gap-8 sm:grid-cols-2", className)}>
       {children}
     </div>
   );
