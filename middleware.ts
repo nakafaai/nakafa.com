3da5894a40493bfc900a4c1a2d060261089da29d
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-04-16 20:56:19 +0200

    revert middleware

--- middleware.ts
+++ middleware.ts
@@ -8,8 +8,5 @@ export const config = {
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … if they start with `/manifest` or are exactly `/manifest.json`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: [
-    "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
-    "/((?!manifest\\.json).*)",
-  ],
+  matcher: "/((?!api|trpc|_next|_vercel|manifest.json|.*\\..*).*)",
 };
