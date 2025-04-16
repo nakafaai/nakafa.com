1df2e7adeca16b875e078ce69b66a672509fb66f
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-04-16 21:52:18 +0200

    revert

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,5 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  // - … opengraph image routes
-  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*|og/.*).*)",
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
 };
