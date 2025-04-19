b649857fabf220d079eb758b7f5fb46c8a47a59b
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-04-19 20:24:44 +0200

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,7 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  // - … if they contain `manifest` or `webmanifest`
-  matcher: "/((?!api|trpc|_next|_vercel|manifest|webmanifest|.*\\..*).*)",
+  // - … if they contain `manifest`, `webmanifest`, or `sitemap`
+  matcher:
+    "/((?!api|trpc|_next|_vercel|manifest|webmanifest|sitemap|.*\\..*).*)",
 };
