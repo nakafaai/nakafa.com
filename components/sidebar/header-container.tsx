f67e5d7eb6ea5e4676ebf682d7574025cfceb01b
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-05-23 16:07:47 +0200

    refactor(sidebar): update header-container styles to improve layout consistency and responsiveness

--- components/sidebar/header-container.tsx
+++ components/sidebar/header-container.tsx
@@ -14,7 +14,7 @@ export function HeaderContainer({ children }: { children: ReactNode }) {
   return (
     <header
       className={cn(
-        "sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-y bg-background/60 backdrop-blur-sm",
+        "sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background/60 backdrop-blur-sm lg:border-t",
         // If the pathname is the home page, don't show the header
         hideHeader && "border-0 lg:hidden"
       )}
