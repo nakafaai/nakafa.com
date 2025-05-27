52a70abe510eba539770d92493aedb54d43bc94e
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-05-27 21:10:29 +0200

    fix(header): add conditional border for search pathname in header container

--- components/sidebar/header-container.tsx
+++ components/sidebar/header-container.tsx
@@ -16,7 +16,8 @@ export function HeaderContainer({ children }: { children: ReactNode }) {
       className={cn(
         "sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background/60 backdrop-blur-sm lg:border-t",
         // If the pathname is the home page, don't show the header
-        hideHeader && "border-0 lg:hidden"
+        hideHeader && "border-0 lg:hidden",
+        pathname === "/search" && "border-b"
       )}
     >
       {children}
