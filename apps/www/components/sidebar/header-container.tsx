3d05d8b56d728cfc42b4af757a509d41b12763f1
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-06-27 20:39:44 +0200

    fix(header): increase background opacity in header for better visibility and aesthetics

--- apps/www/components/sidebar/header-container.tsx
+++ apps/www/components/sidebar/header-container.tsx
@@ -9,7 +9,7 @@ export function HeaderContainer({ children }: { children: ReactNode }) {
   const borderOpacity = useTransform(scrollY, [0, 50], [0, 1]);
 
   return (
-    <motion.header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 bg-background/30 backdrop-blur-xs lg:hidden">
+    <motion.header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 bg-background/80 backdrop-blur-xs lg:hidden">
       <motion.div
         className="absolute right-0 bottom-0 left-0 h-[1px] bg-border"
         style={{ opacity: borderOpacity }}
