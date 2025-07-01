9184fbdc24a777fb50266ba8965cff24d6a8ad04
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-07-01 17:37:28 +0200

    feat(contents): add base path for high school geography subject

---
+++ packages/contents/subject/high-school/11/geography/_data/en-material.ts
@@ -0,0 +1,383 @@
+import type { MaterialList } from "@repo/contents/_types/subject/material";
+import { BASE_PATH } from ".";
+
+const enMaterials: MaterialList = [
+  {
+    title: "Indonesia's Strategic Position and Natural Resource Potential",
+    description:
+      "Emerald jewel of the equator with abundant natural wealth determining the nation's future.",
+    href: `${BASE_PATH}/indonesia-position`,
+    items: [
+      {
+        title: "Understanding Indonesia's Astronomical Location",
+        href: `${BASE_PATH}/indonesia-position/astronomical`,
+      },
+      {
+        title: "Astronomical Location's Impact on Indonesia's Tropical Climate",
+        href: `${BASE_PATH}/indonesia-position/astronomical-tropical-climate`,
+      },
+      {
+        title:
+          "Astronomical Location's Influence on Indonesia's Time Zone Division",
+        href: `${BASE_PATH}/indonesia-position/astronomical-location-time-zone`,
+      },
+      {
+        title: "Benefits of Astronomical Location for Indonesia",
+        href: `${BASE_PATH}/indonesia-position/astronomical-benefits`,
+      },
+      {
+        title: "Understanding Indonesia's Geographical Location",
+        href: `${BASE_PATH}/indonesia-position/geographical`,
+      },
+      {
+        title: "Geographical Location's Influence on Indonesia's Climate",
+        href: `${BASE_PATH}/indonesia-position/geographical-climate`,
+      },
+      {
+        title: "Indonesia's Geographical Location at World Traffic Crossroads",
+        href: `${BASE_PATH}/indonesia-position/geographical-world-crossroads`,
+      },
+      {
+        title: "Benefits of Indonesia's Geographical Location",
+        href: `${BASE_PATH}/indonesia-position/geographical-benefits`,
+      },
+      {
+        title: "Understanding Geological Location",
+        href: `${BASE_PATH}/indonesia-position/geological`,
+      },
+      {
+        title: "Indonesia's Geological Location Impact",
+        href: `${BASE_PATH}/indonesia-position/geological-impact`,
+      },
+      {
+        title: "Managing Indonesia's Geographical Potential",
+        href: `${BASE_PATH}/indonesia-position/geographical-management`,
+      },
+      {
+        title: "Definition of Natural Resources",
+        href: `${BASE_PATH}/indonesia-position/resource-definition`,
+      },
+      {
+        title: "Natural Resource Classification",
+        href: `${BASE_PATH}/indonesia-position/resource-classification`,
+      },
+      {
+        title: "Natural Resources Impact on Life",
+        href: `${BASE_PATH}/indonesia-position/resource-impact`,
+      },
+      {
+        title: "Indonesia's Natural Resource Potential",
+        href: `${BASE_PATH}/indonesia-position/indonesia-resource-potential`,
+      },
+      {
+        title: "Indonesia's Natural Resource Distribution",
+        href: `${BASE_PATH}/indonesia-position/indonesia-resource-distribution`,
+      },
+      {
+        title: "Sustainable Natural Resource Management",
+        href: `${BASE_PATH}/indonesia-position/sustainable-resource-management`,
+      },
+      {
+        title: "Natural Resource Management Problems",
+        href: `${BASE_PATH}/indonesia-position/resource-management-problems`,
+      },
+    ],
+  },
+  {
+    title: "Biological Diversity",
+    description:
+      "Biodiversity paradise serving as home to millions of unique species on Earth.",
+    href: `${BASE_PATH}/biological-diversity`,
+    items: [
+      {
+        title: "Definition of Flora and Fauna",
+        href: `${BASE_PATH}/biological-diversity/flora-fauna-definition`,
+      },
+      {
+        title: "Importance of Flora and Fauna for Life",
+        href: `${BASE_PATH}/biological-diversity/flora-fauna-importance`,
+      },
+      {
+        title: "Diversity and Dynamics of Indonesian Flora and Fauna",
+        href: `${BASE_PATH}/biological-diversity/indonesia-flora-fauna-diversity`,
+      },
+      {
+        title: "Problems of Indonesian Flora and Fauna",
+        href: `${BASE_PATH}/biological-diversity/flora-fauna-problems`,
+      },
+      {
+        title: "Biome System Distribution",
+        href: `${BASE_PATH}/biological-diversity/biome-distribution`,
+      },
+      {
+        title: "World Flora and Fauna Distribution",
+        href: `${BASE_PATH}/biological-diversity/world-flora-fauna-distribution`,
+      },
+      {
+        title: "Indonesian Flora Distribution",
+        href: `${BASE_PATH}/biological-diversity/indonesia-flora-distribution`,
+      },
+      {
+        title: "Indonesian Fauna Distribution",
+        href: `${BASE_PATH}/biological-diversity/indonesia-fauna-distribution`,
+      },
+      {
+        title: "Factors Influencing Flora and Fauna Distribution",
+        href: `${BASE_PATH}/biological-diversity/factors-influencing-flora-fauna-distribution`,
+      },
+      {
+        title: "Flora and Fauna Conservation for Human Welfare",
+        href: `${BASE_PATH}/biological-diversity/conservation-flora-fauna-human`,
+      },
+      {
+        title: "Flora and Fauna Conservation Methods",
+        href: `${BASE_PATH}/biological-diversity/conservation-methods`,
+      },
+      {
+        title: "Flora and Fauna Conservation Practices",
+        href: `${BASE_PATH}/biological-diversity/conservation-practices`,
+      },
+    ],
+  },
+  {
+    title: "Environment and Population",
+    description:
+      "Dynamic relationship between humans and nature shaping the planet's future.",
+    href: `${BASE_PATH}/environment-population`,
+    items: [
+      {
+        title: "Understanding Environment, Ecosystem, and Environmental Ethics",
+        href: `${BASE_PATH}/environment-population/ecosystem-environmental-ethics`,
+      },
+      {
+        title: "Biotic Environment",
+        href: `${BASE_PATH}/environment-population/biotic-environment`,
+      },
+      {
+        title: "Abiotic Environment",
+        href: `${BASE_PATH}/environment-population/abiotic-environment`,
+      },
+      {
+        title: "Social Cultural Environment",
+        href: `${BASE_PATH}/environment-population/social-cultural-environment`,
+      },
+      {
+        title: "Environmental Benefits",
+        href: `${BASE_PATH}/environment-population/environment-benefits`,
+      },
+      {
+        title: "Environmental Quality and Its Importance for Life",
+        href: `${BASE_PATH}/environment-population/environment-quality-importance`,
+      },
+      {
+        title: "Environmental Quality Index",
+        href: `${BASE_PATH}/environment-population/environment-quality-index`,
+      },
+      {
+        title: "Factors Influencing Environmental Quality",
+        href: `${BASE_PATH}/environment-population/factors-influencing-environment-quality`,
+      },
+      {
+        title: "Global Warming",
+        href: `${BASE_PATH}/environment-population/global-warming`,
+      },
+      {
+        title: "Air Pollution",
+        href: `${BASE_PATH}/environment-population/air-pollution`,
+      },
+      {
+        title: "Water Pollution",
+        href: `${BASE_PATH}/environment-population/water-pollution`,
+      },
+      {
+        title: "Coastal and Marine Pollution",
+        href: `${BASE_PATH}/environment-population/coastal-and-marine-pollution`,
+      },
+      {
+        title: "Soil Pollution",
+        href: `${BASE_PATH}/environment-population/soil-pollution`,
+      },
+      {
+        title: "Population Increase",
+        href: `${BASE_PATH}/environment-population/population-increase`,
+      },
+      {
+        title: "Definition of Population",
+        href: `${BASE_PATH}/environment-population/population-definition`,
+      },
+      {
+        title: "World and Indonesian Population Growth Over Time",
+        href: `${BASE_PATH}/environment-population/world-indonesia-population-increase`,
+      },
+      {
+        title: "Population Growth and Calculation Methods",
+        href: `${BASE_PATH}/environment-population/population-growth-calculation`,
+      },
+      {
+        title: "Population Data",
+        href: `${BASE_PATH}/environment-population/population-data`,
+      },
+      {
+        title: "Definition of Human Resources",
+        href: `${BASE_PATH}/environment-population/human-resource-definition`,
+      },
+      {
+        title: "Human Resource Quantity and Quality",
+        href: `${BASE_PATH}/environment-population/human-resource-quantity-quality`,
+      },
+      {
+        title: "Population Distribution",
+        href: `${BASE_PATH}/environment-population/population-distribution`,
+      },
+      {
+        title: "Population Composition",
+        href: `${BASE_PATH}/environment-population/population-composition`,
+      },
+      {
+        title: "Health",
+        href: `${BASE_PATH}/environment-population/health`,
+      },
+      {
+        title: "Education",
+        href: `${BASE_PATH}/environment-population/education`,
+      },
+      {
+        title: "Human Resource Development",
+        href: `${BASE_PATH}/environment-population/human-resource-development`,
+      },
+      {
+        title: "Population Problems",
+        href: `${BASE_PATH}/environment-population/population-problems`,
+      },
+      {
+        title: "Implementing Family Planning Programs",
+        href: `${BASE_PATH}/environment-population/family-planning-program`,
+      },
+      {
+        title: "Health Sector Services",
+        href: `${BASE_PATH}/environment-population/health-sector-service`,
+      },
+      {
+        title: "Transmigration",
+        href: `${BASE_PATH}/environment-population/transmigration`,
+      },
+      {
+        title: "Improving Education and Skills Quality",
+        href: `${BASE_PATH}/environment-population/education-skill-improvement`,
+      },
+      {
+        title: "Improving Agricultural Production",
+        href: `${BASE_PATH}/environment-population/agriculture-production-improvement`,
+      },
+    ],
+  },
+  {
+    title: "Disaster Mitigation and Adaptation",
+    description: "Survival strategies against unpredictable forces of nature.",
+    href: `${BASE_PATH}/disaster-mitigation-adaptation`,
+    items: [
+      {
+        title: "Vulnerability",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/vulnerability`,
+      },
+      {
+        title: "Threats",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/threat`,
+      },
+      {
+        title: "Capacity",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/capacity`,
+      },
+      {
+        title: "Natural Disasters",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/natural-disaster`,
+      },
+      {
+        title: "Non-Natural Disasters",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/non-natural-disaster`,
+      },
+      {
+        title: "Social Disasters",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/social-disaster`,
+      },
+      {
+        title: "Volcanic Eruptions",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/volcanic-eruption`,
+      },
+      {
+        title: "Landslides",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/landslide`,
+      },
+      {
+        title: "Earthquakes",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/earthquake`,
+      },
+      {
+        title: "Droughts",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/drought`,
+      },
+      {
+        title: "Floods",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/flood`,
+      },
+      {
+        title: "Forest Fires",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/forest-fire`,
+      },
+      {
+        title: "Tsunamis",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/tsunami`,
+      },
+      {
+        title: "Other Disasters",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/other-disasters`,
+      },
+      {
+        title: "Tornadoes",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/dust-storm`,
+      },
+      {
+        title: "Typhoons",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/typhoon`,
+      },
+      {
+        title: "Tsunami Mitigation",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/tsunami-mitigation`,
+      },
+      {
+        title: "Volcanic Eruption Mitigation",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/volcanic-eruption-mitigation`,
+      },
+      {
+        title: "Earthquake Mitigation",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/earthquake-mitigation`,
+      },
+      {
+        title: "Landslide Mitigation",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/landslide-mitigation`,
+      },
+      {
+        title: "Flood Mitigation",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/flood-mitigation`,
+      },
+      {
+        title: "Drought Mitigation",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/drought-mitigation`,
+      },
+      {
+        title: "Natural Disaster Adaptation",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/natural-disaster-adaptation`,
+      },
+      {
+        title: "Non-Natural Disaster Adaptation",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/non-natural-disaster-adaptation`,
+      },
+      {
+        title: "Social Disaster Adaptation",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/social-disaster-adaptation`,
+      },
+    ],
+  },
+] as const;
+
+export default enMaterials;
---
+++ packages/contents/subject/high-school/11/geography/_data/id-material.ts
@@ -0,0 +1,386 @@
+import type { MaterialList } from "@repo/contents/_types/subject/material";
+import { BASE_PATH } from ".";
+
+const idMaterials: MaterialList = [
+  {
+    title: "Posisi Strategis Indonesia dan Potensi Sumber Daya Alam",
+    description:
+      "Permata zamrud khatulistiwa dengan kekayaan alam melimpah yang menentukan masa depan bangsa.",
+    href: `${BASE_PATH}/indonesia-position`,
+    items: [
+      {
+        title: "Pengertian Letak Indonesia Secara Astronomis",
+        href: `${BASE_PATH}/indonesia-position/astronomical`,
+      },
+      {
+        title:
+          "Pengertian Letak Astronomis Terhadap Iklim Indonesia yang Bercorak Tropis",
+        href: `${BASE_PATH}/indonesia-position/astronomical-tropical-climate`,
+      },
+      {
+        title:
+          "Pengaruh Letak Astronomis terhadap Pembagian Zona Waktu di Indonesia",
+        href: `${BASE_PATH}/indonesia-position/astronomical-location-time-zone`,
+      },
+      {
+        title: "Keuntungan Letak Astronomis untuk Indonesia",
+        href: `${BASE_PATH}/indonesia-position/astronomical-benefits`,
+      },
+      {
+        title: "Pengertian Letak Indonesia Secara Geografis",
+        href: `${BASE_PATH}/indonesia-position/geographical`,
+      },
+      {
+        title: "Pengaruh Letak Indonesia Secara Geografis Terhadap Iklim",
+        href: `${BASE_PATH}/indonesia-position/geographical-climate`,
+      },
+      {
+        title:
+          "Pengertian Letak Geografis Indonesia di Persimpangan Lalu Lintas Dunia",
+        href: `${BASE_PATH}/indonesia-position/geographical-world-crossroads`,
+      },
+      {
+        title: "Keuntungan Letak Indonesia Secara Geografis",
+        href: `${BASE_PATH}/indonesia-position/geographical-benefits`,
+      },
+      {
+        title: "Pengertian Letak Geologis",
+        href: `${BASE_PATH}/indonesia-position/geological`,
+      },
+      {
+        title: "Pengaruh Letak Indonesia Secara Geologis",
+        href: `${BASE_PATH}/indonesia-position/geological-impact`,
+      },
+      {
+        title: "Pengelolaan Potensi Geografis Indonesia",
+        href: `${BASE_PATH}/indonesia-position/geographical-management`,
+      },
+      {
+        title: "Pengertian Sumber Daya Alam",
+        href: `${BASE_PATH}/indonesia-position/resource-definition`,
+      },
+      {
+        title: "Klasifikasi Sumber Daya Alam",
+        href: `${BASE_PATH}/indonesia-position/resource-classification`,
+      },
+      {
+        title: "Pengaruh Sumber Daya Alam Terhadap Kehidupan",
+        href: `${BASE_PATH}/indonesia-position/resource-impact`,
+      },
+      {
+        title: "Potensi Sumber Daya Alam Indonesia",
+        href: `${BASE_PATH}/indonesia-position/indonesia-resource-potential`,
+      },
+      {
+        title: "Sebaran Sumber Daya Alam Indonesia",
+        href: `${BASE_PATH}/indonesia-position/indonesia-resource-distribution`,
+      },
+      {
+        title: "Pengelolaan Sumber Daya Alam yang Berkelanjutan",
+        href: `${BASE_PATH}/indonesia-position/sustainable-resource-management`,
+      },
+      {
+        title: "Permasalahan Pengelolaan Sumber Daya Alam",
+        href: `${BASE_PATH}/indonesia-position/resource-management-problems`,
+      },
+    ],
+  },
+  {
+    title: "Keragaman Hayati",
+    description:
+      "Surga biodiversitas yang menjadi rumah bagi jutaan spesies unik di bumi.",
+    href: `${BASE_PATH}/biological-diversity`,
+    items: [
+      {
+        title: "Pengertian Flora dan Fauna",
+        href: `${BASE_PATH}/biological-diversity/flora-fauna-definition`,
+      },
+      {
+        title: "Arti Penting Flora dan Fauna bagi Kehidupan",
+        href: `${BASE_PATH}/biological-diversity/flora-fauna-importance`,
+      },
+      {
+        title: "Ragam dan Dinamika Jenis Flora dan Fauna Indonesia",
+        href: `${BASE_PATH}/biological-diversity/indonesia-flora-fauna-diversity`,
+      },
+      {
+        title: "Permasalahan Flora dan Fauna Indonesia",
+        href: `${BASE_PATH}/biological-diversity/flora-fauna-problems`,
+      },
+      {
+        title: "Persebaran Sistem Bioma",
+        href: `${BASE_PATH}/biological-diversity/biome-distribution`,
+      },
+      {
+        title: "Persebaran Flora dan Fauna di Dunia",
+        href: `${BASE_PATH}/biological-diversity/world-flora-fauna-distribution`,
+      },
+      {
+        title: "Persebaran Flora Indonesia",
+        href: `${BASE_PATH}/biological-diversity/indonesia-flora-distribution`,
+      },
+      {
+        title: "Persebaran Fauna Indonesia",
+        href: `${BASE_PATH}/biological-diversity/indonesia-fauna-distribution`,
+      },
+      {
+        title: "Faktor yang Mempengaruhi Sebaran Flora dan Fauna",
+        href: `${BASE_PATH}/biological-diversity/factors-influencing-flora-fauna-distribution`,
+      },
+      {
+        title: "Pelestarian Flora dan Fauna untuk Kesejahteraan Manusia",
+        href: `${BASE_PATH}/biological-diversity/conservation-flora-fauna-human`,
+      },
+      {
+        title: "Metode Pelestarian Flora dan Fauna",
+        href: `${BASE_PATH}/biological-diversity/conservation-methods`,
+      },
+      {
+        title: "Praktik Pelestarian Flora dan Fauna",
+        href: `${BASE_PATH}/biological-diversity/conservation-practices`,
+      },
+    ],
+  },
+  {
+    title: "Lingkungan dan Kependudukan",
+    description:
+      "Hubungan dinamis antara manusia dan alam yang membentuk masa depan planet.",
+    href: `${BASE_PATH}/environment-population`,
+    items: [
+      {
+        title: "Pengertian Lingkungan, Ekosistem, dan Etika Lingkungan",
+        href: `${BASE_PATH}/environment-population/ecosystem-environmental-ethics`,
+      },
+      {
+        title: "Lingkungan Biotik",
+        href: `${BASE_PATH}/environment-population/biotic-environment`,
+      },
+      {
+        title: "Lingkungan Abiotik",
+        href: `${BASE_PATH}/environment-population/abiotic-environment`,
+      },
+      {
+        title: "Lingkungan Sosial Budaya",
+        href: `${BASE_PATH}/environment-population/social-cultural-environment`,
+      },
+      {
+        title: "Manfaat Lingkungan",
+        href: `${BASE_PATH}/environment-population/environment-benefits`,
+      },
+      {
+        title: "Pengertian Kualitas Lingkungan dan Pentingnya bagi Kehidupan",
+        href: `${BASE_PATH}/environment-population/environment-quality-importance`,
+      },
+      {
+        title: "Indeks Kualitas Lingkungan Hidup",
+        href: `${BASE_PATH}/environment-population/environment-quality-index`,
+      },
+      {
+        title: "Faktor yang Mempengaruhi Kualitas Lingkungan",
+        href: `${BASE_PATH}/environment-population/factors-influencing-environment-quality`,
+      },
+      {
+        title: "Global Warming",
+        href: `${BASE_PATH}/environment-population/global-warming`,
+      },
+      {
+        title: "Pencemaran Udara",
+        href: `${BASE_PATH}/environment-population/air-pollution`,
+      },
+      {
+        title: "Pencemaran Air",
+        href: `${BASE_PATH}/environment-population/water-pollution`,
+      },
+      {
+        title: "Pencemaran Pantai dan Laut",
+        href: `${BASE_PATH}/environment-population/coastal-and-marine-pollution`,
+      },
+      {
+        title: "Pencemaran Tanah",
+        href: `${BASE_PATH}/environment-population/soil-pollution`,
+      },
+      {
+        title: "Peningkatan Jumlah Populasi",
+        href: `${BASE_PATH}/environment-population/population-increase`,
+      },
+      {
+        title: "Pengertian Penduduk",
+        href: `${BASE_PATH}/environment-population/population-definition`,
+      },
+      {
+        title: "Pertambahan Penduduk Dunia dan Indonesia dari Waktu ke Waktu",
+        href: `${BASE_PATH}/environment-population/world-indonesia-population-increase`,
+      },
+      {
+        title: "Pertumbuhan Penduduk dan Cara Menghitungnya",
+        href: `${BASE_PATH}/environment-population/population-growth-calculation`,
+      },
+      {
+        title: "Data Kependudukan",
+        href: `${BASE_PATH}/environment-population/population-data`,
+      },
+      {
+        title: "Pengertian Sumber Daya Manusia",
+        href: `${BASE_PATH}/environment-population/human-resource-definition`,
+      },
+      {
+        title: "Kuantitas dan Kualitas Sumber Daya Manusia",
+        href: `${BASE_PATH}/environment-population/human-resource-quantity-quality`,
+      },
+      {
+        title: "Distribusi Penduduk",
+        href: `${BASE_PATH}/environment-population/population-distribution`,
+      },
+      {
+        title: "Komposisi Penduduk",
+        href: `${BASE_PATH}/environment-population/population-composition`,
+      },
+      {
+        title: "Kesehatan",
+        href: `${BASE_PATH}/environment-population/health`,
+      },
+      {
+        title: "Pendidikan",
+        href: `${BASE_PATH}/environment-population/education`,
+      },
+      {
+        title: "Pengembangan Sumber Daya Manusia",
+        href: `${BASE_PATH}/environment-population/human-resource-development`,
+      },
+      {
+        title: "Masalah Kependudukan",
+        href: `${BASE_PATH}/environment-population/population-problems`,
+      },
+      {
+        title: "Melaksanakan Program Keluarga Berencana",
+        href: `${BASE_PATH}/environment-population/family-planning-program`,
+      },
+      {
+        title: "Pelayanan Sektor Kesehatan",
+        href: `${BASE_PATH}/environment-population/health-sector-service`,
+      },
+      {
+        title: "Transmigrasi",
+        href: `${BASE_PATH}/environment-population/transmigration`,
+      },
+      {
+        title: "Meningkatkan Mutu Pendidikan dan Keterampilan",
+        href: `${BASE_PATH}/environment-population/education-skill-improvement`,
+      },
+      {
+        title: "Meningkatkan Produksi Pertanian",
+        href: `${BASE_PATH}/environment-population/agriculture-production-improvement`,
+      },
+    ],
+  },
+  {
+    title: "Mitigasi dan Adaptasi Kebencanaan",
+    description:
+      "Strategi bertahan hidup melawan kekuatan alam yang tak terduga.",
+    href: `${BASE_PATH}/disaster-mitigation-adaptation`,
+    items: [
+      {
+        title: "Kerentanan",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/vulnerability`,
+      },
+      {
+        title: "Ancaman",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/threat`,
+      },
+      {
+        title: "Kapasitas",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/capacity`,
+      },
+      {
+        title: "Bencana Alam",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/natural-disaster`,
+      },
+      {
+        title: "Bencana Non Alam",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/non-natural-disaster`,
+      },
+      {
+        title: "Bencana Sosial",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/social-disaster`,
+      },
+      {
+        title: "Letusan Gunung Berapi",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/volcanic-eruption`,
+      },
+      {
+        title: "Tanah Longsor",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/landslide`,
+      },
+      {
+        title: "Gempa Bumi",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/earthquake`,
+      },
+      {
+        title: "Kekeringan",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/drought`,
+      },
+      {
+        title: "Banjir",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/flood`,
+      },
+      {
+        title: "Kebakaran Hutan",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/forest-fire`,
+      },
+      {
+        title: "Tsunami",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/tsunami`,
+      },
+      {
+        title: "Bencana Lainnya",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/other-disasters`,
+      },
+      {
+        title: "Angin Puting Beliung",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/dust-storm`,
+      },
+      {
+        title: "Angin Topan",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/typhoon`,
+      },
+      {
+        title: "Mitigasi Tsunami",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/tsunami-mitigation`,
+      },
+      {
+        title: "Mitigasi Gunung Berapi",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/volcanic-eruption-mitigation`,
+      },
+      {
+        title: "Mitigasi Gempa Bumi",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/earthquake-mitigation`,
+      },
+      {
+        title: "Mitigasi Tanah Longsor",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/landslide-mitigation`,
+      },
+      {
+        title: "Mitigasi Banjir",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/flood-mitigation`,
+      },
+      {
+        title: "Mitigasi Kekeringan",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/drought-mitigation`,
+      },
+      {
+        title: "Adaptasi Bencana Alam",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/natural-disaster-adaptation`,
+      },
+      {
+        title: "Adaptasi Bencana Non Alam",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/non-natural-disaster-adaptation`,
+      },
+      {
+        title: "Adaptasi Bencana Sosial",
+        href: `${BASE_PATH}/disaster-mitigation-adaptation/social-disaster-adaptation`,
+      },
+    ],
+  },
+] as const;
+
+export default idMaterials;
