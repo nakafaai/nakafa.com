b75d87e4e8c7e97e2fef1c8f09c80f998ba4f69b
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-07-18 14:23:06 +0200

    refactor: make SurahSchema optional to enhance flexibility in Quran type definitions

--- packages/contents/_types/quran.ts
+++ packages/contents/_types/quran.ts
@@ -64,7 +64,7 @@ export const SurahSchema = z.object({
     text: true,
     translation: true,
     audio: true,
-  }),
+  }).optional(),
   verses: z.array(VerseSchema),
 });
 export type Surah = z.infer<typeof SurahSchema>;
