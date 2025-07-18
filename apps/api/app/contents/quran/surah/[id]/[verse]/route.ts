8b42d07d0a3765db7d65e953c86592f8e4e7e4d2
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-07-18 02:45:44 +0200

    refactor: implement generateStaticParams function to generate dynamic parameters for all surahs and verses in route.ts

--- apps/api/app/contents/quran/surah/[id]/[verse]/route.ts
+++ apps/api/app/contents/quran/surah/[id]/[verse]/route.ts
@@ -1,5 +1,22 @@
 import { NextResponse } from "next/server";
 
+export function generateStaticParams() {
+  const ids: string[] = [];
+  const verses: string[] = [];
+
+  // 114 surah
+  for (let i = 1; i <= 114; i++) {
+    ids.push(i.toString());
+  }
+
+  // 6236 verses
+  for (let i = 1; i <= 6236; i++) {
+    verses.push(i.toString());
+  }
+
+  return ids.flatMap((id) => verses.map((verse) => ({ id, verse })));
+}
+
 // can not support static params for 114 surah and 6236 verses = 710.904 params
 export async function GET(
   _req: Request,
