95dbed8033f3412555d7873912c2b1de02592a6e
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-07-23 22:22:45 +0200

    chore: update redis to version 5.6.1 and react-aria-components to version 1.11.0; update @react-three/drei to version 10.6.0 and motion to version 12.23.7 for improved performance and compatibility across multiple packages

--- packages/design-system/index.tsx
+++ packages/design-system/index.tsx
@@ -1,6 +1,5 @@
 import type { ThemeProviderProps } from "next-themes";
 import { Toaster } from "./components/ui/sonner";
-import { TooltipProvider } from "./components/ui/tooltip";
 import { ThemeProvider } from "./providers/theme";
 
 type Properties = ThemeProviderProps;
@@ -8,7 +7,7 @@ type Properties = ThemeProviderProps;
 export function DesignSystemProvider({ children, ...properties }: Properties) {
   return (
     <ThemeProvider {...properties}>
-      <TooltipProvider>{children}</TooltipProvider>
+      {children}
       <Toaster />
     </ThemeProvider>
   );
