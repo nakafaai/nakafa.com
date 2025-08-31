725b0657786676ed4f0769ac8ca91d70ccfc30cc
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-08-31 23:17:51 +0200

    fix: update logout button to redirect to home after sign out for improved user experience

--- apps/www/components/auth/logout.tsx
+++ apps/www/components/auth/logout.tsx
@@ -2,15 +2,19 @@
 
 import { useAuthActions } from "@convex-dev/auth/react";
 import { Button } from "@repo/design-system/components/ui/button";
+import { useRouter } from "@repo/internationalization/src/navigation";
 import { LogOutIcon } from "lucide-react";
 import { useTranslations } from "next-intl";
 
 export function AuthLogout() {
   const t = useTranslations("Auth");
+
+  const router = useRouter();
+
   const { signOut } = useAuthActions();
 
   return (
-    <Button onClick={() => signOut()}>
+    <Button onClick={() => signOut().then(() => router.replace("/"))}>
       <LogOutIcon />
       {t("logout")}
     </Button>
