5c672020622d511496896787fb49cb2c2d1244e1
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-10-22 20:56:24 +0200

    refactor: remove cacheComponents option from next-config for improved clarity and performance

--- packages/next-config/index.ts
+++ packages/next-config/index.ts
@@ -14,7 +14,6 @@ export const config: NextConfig = {
   pageExtensions: ["mdx", "ts", "tsx"],
   reactCompiler: true,
   serverExternalPackages: ["shiki"],
-  cacheComponents: true,
   experimental: {
     optimizePackageImports: ["three"],
   },
