4118eb8f10afc6cf178dea400a885e6bd2f70e2a
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-11-11 14:25:14 +0100

    refactor: reorganize component structure and enhance code clarity for improved maintainability

---
+++ packages/contents/exercises/high-school/tka/mathematics/try-out/set-2/36/choices.ts
@@ -0,0 +1,51 @@
+import type { ExercisesChoices } from "@repo/contents/_types/exercises/choices";
+
+const choices: ExercisesChoices = {
+  id: [
+    {
+      label: "$$-\\frac{13}{9}$$",
+      value: true,
+    },
+    {
+      label: "$$-\\frac{11}{9}$$",
+      value: false,
+    },
+    {
+      label: "$$-1$$",
+      value: false,
+    },
+    {
+      label: "$$-\\frac{7}{9}$$",
+      value: false,
+    },
+    {
+      label: "$$-\\frac{5}{9}$$",
+      value: false,
+    },
+  ],
+  en: [
+    {
+      label: "$$-\\frac{13}{9}$$",
+      value: true,
+    },
+    {
+      label: "$$-\\frac{11}{9}$$",
+      value: false,
+    },
+    {
+      label: "$$-1$$",
+      value: false,
+    },
+    {
+      label: "$$-\\frac{7}{9}$$",
+      value: false,
+    },
+    {
+      label: "$$-\\frac{5}{9}$$",
+      value: false,
+    },
+  ],
+};
+
+export default choices;
+
---
+++ packages/contents/exercises/high-school/tka/mathematics/try-out/set-2/37/choices.ts
@@ -0,0 +1,51 @@
+import type { ExercisesChoices } from "@repo/contents/_types/exercises/choices";
+
+const choices: ExercisesChoices = {
+  id: [
+    {
+      label: "$$\\frac{61}{5\\sqrt{5}}$$",
+      value: false,
+    },
+    {
+      label: "$$\\frac{8}{5\\sqrt{5}}$$",
+      value: false,
+    },
+    {
+      label: "$$\\frac{3}{5\\sqrt{5}}$$",
+      value: false,
+    },
+    {
+      label: "$$\\frac{8}{125}$$",
+      value: false,
+    },
+    {
+      label: "$$\\frac{3}{125}$$",
+      value: true,
+    },
+  ],
+  en: [
+    {
+      label: "$$\\frac{61}{5\\sqrt{5}}$$",
+      value: false,
+    },
+    {
+      label: "$$\\frac{8}{5\\sqrt{5}}$$",
+      value: false,
+    },
+    {
+      label: "$$\\frac{3}{5\\sqrt{5}}$$",
+      value: false,
+    },
+    {
+      label: "$$\\frac{8}{125}$$",
+      value: false,
+    },
+    {
+      label: "$$\\frac{3}{125}$$",
+      value: true,
+    },
+  ],
+};
+
+export default choices;
+
