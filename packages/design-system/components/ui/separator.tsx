6735c310216677de8ea50b9ab11427da6c5da6ae
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2025-11-18 22:15:56 +0100

    refactor: update Separator component to use new base-ui-components library and improve class handling

--- packages/design-system/components/ui/separator.tsx
+++ packages/design-system/components/ui/separator.tsx
@@ -1,23 +1,19 @@
-"use client";
+import { Separator as SeparatorPrimitive } from "@base-ui-components/react/separator";
 
 import { cn } from "@repo/design-system/lib/utils";
-import { Separator as SeparatorPrimitive } from "radix-ui";
-import type * as React from "react";
 
 function Separator({
   className,
   orientation = "horizontal",
-  decorative = true,
   ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}: SeparatorPrimitive.Props) {
   return (
-    <SeparatorPrimitive.Root
+    <SeparatorPrimitive
       className={cn(
-        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=vertical]:h-full data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px",
+        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px data-[orientation=vertical]:not-[[class^='h-']]:not-[[class*='_h-']]:self-stretch",
         className
       )}
-      data-slot="separator-root"
-      decorative={decorative}
+      data-slot="separator"
       orientation={orientation}
       {...props}
     />
