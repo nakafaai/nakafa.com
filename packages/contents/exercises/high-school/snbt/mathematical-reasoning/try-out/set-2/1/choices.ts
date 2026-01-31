5c9d56f66a0edcd0c3c2c0cc64363a858b65c187
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2026-01-31 14:32:53 +0100

    feat(contents): add new exercise set for mathematical reasoning
    Add "Set 2" to the exercise materials for high school SNBT mathematical reasoning.
    This includes the first question in both English and Indonesian, along with its
    multiple-choice options and detailed answer explanations.

---
+++ packages/contents/exercises/high-school/snbt/mathematical-reasoning/try-out/set-2/1/choices.ts
@@ -0,0 +1,20 @@
+import type { ExercisesChoices } from "@repo/contents/_types/exercises/choices";
+
+const choices: ExercisesChoices = {
+  id: [
+    { label: "$$30\\text{ m}^2$$", value: false },
+    { label: "$$60\\text{ m}^2$$", value: true },
+    { label: "$$70\\text{ m}^2$$", value: false },
+    { label: "$$80\\text{ m}^2$$", value: false },
+    { label: "$$90\\text{ m}^2$$", value: false },
+  ],
+  en: [
+    { label: "$$30\\text{ m}^2$$", value: false },
+    { label: "$$60\\text{ m}^2$$", value: true },
+    { label: "$$70\\text{ m}^2$$", value: false },
+    { label: "$$80\\text{ m}^2$$", value: false },
+    { label: "$$90\\text{ m}^2$$", value: false },
+  ],
+};
+
+export default choices;
