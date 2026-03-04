a95bef5b613f4f23c5b1badd3f130bb5cf18ea0b
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2026-03-05 00:05:56 +0100

    Add exercises 19-20 questions, answers, and choices

---
+++ packages/contents/exercises/high-school/snbt/general-knowledge/try-out/set-2/19/choices.ts
@@ -0,0 +1,60 @@
+import type { ExercisesChoices } from "@repo/contents/_types/exercises/choices";
+
+export const choices: ExercisesChoices = {
+  id: [
+    {
+      label:
+        "menambah jumlah penyuluh di bidang perikanan budi daya agar pertumbuhan bidang budi daya ikan makin optimal.",
+      value: false,
+    },
+    {
+      label:
+        "membenahi sumber daya manusia, membenahi manajemen perikanan, dan meningkatkan teknologi budi daya perikanan.",
+      value: true,
+    },
+    {
+      label:
+        "memperbaiki kualitas air di kawasan padat penduduk dan di kawasan yang tingkat intensitas pembangunannya tinggi.",
+      value: false,
+    },
+    {
+      label:
+        "menambah jumlah alat tangkap ikan dan memberikan subsidi kepada pelaku usaha perikanan agar jumlah produksi meningkat.",
+      value: false,
+    },
+    {
+      label:
+        "memperhatikan ambang batas baku mutu air buangan limbah agar hasil budi daya ikan meningkat.",
+      value: false,
+    },
+  ],
+  en: [
+    {
+      label:
+        "increase the number of extension workers in the field of aquaculture so that the growth of the fish farming sector becomes more optimal.",
+      value: false,
+    },
+    {
+      label:
+        "improve human resources, improve fisheries management, and improve aquaculture technology.",
+      value: true,
+    },
+    {
+      label:
+        "improve water quality in densely populated areas and in areas with a high intensity of development.",
+      value: false,
+    },
+    {
+      label:
+        "increase the number of fishing gears and provide subsidies to fishery business actors so that production volume increases.",
+      value: false,
+    },
+    {
+      label:
+        "pay attention to the threshold for wastewater quality standards so that fish farming yields increase.",
+      value: false,
+    },
+  ],
+};
+
+export default choices;
---
+++ packages/contents/exercises/high-school/snbt/general-knowledge/try-out/set-2/20/choices.ts
@@ -0,0 +1,60 @@
+import type { ExercisesChoices } from "@repo/contents/_types/exercises/choices";
+
+export const choices: ExercisesChoices = {
+  id: [
+    {
+      label:
+        "Di samping itu, produktivitas usaha perikanan budi daya juga belum optimal.",
+      value: false,
+    },
+    {
+      label:
+        "Pengetahuan petani dan nelayan tentang teknologi budi daya masih rendah, misalnya dalam pemilihan induk ikan, pemijahan, penetesan, pembuahan, pemeliharaan larva, pendederan, dan pembesaran.",
+      value: false,
+    },
+    {
+      label:
+        "Tenaga penyuluh perikanan juga masih kurang sehingga inovasi teknologi sulit ditingkatkan.",
+      value: true,
+    },
+    {
+      label:
+        "Berkaitan dengan penggunaan lahan, antara usaha budi daya perikanan dan kegiatan pembangunan lainnya (seperti permukiman, industri, dan pertambangan) sering merugikan usaha budi daya perikanan, termasuk memburuknya kualitas air.",
+      value: false,
+    },
+    {
+      label:
+        "Peningkatan tertinggi didominasi penangkapan ikan di laut, yaitu dari 4.383.103 ton pada tahun 2003 menjadi 4.647.730 ton pada tahun 2007.",
+      value: false,
+    },
+  ],
+  en: [
+    {
+      label:
+        "Besides that, the productivity of aquaculture businesses is also not yet optimal.",
+      value: false,
+    },
+    {
+      label:
+        "The knowledge of farmers and fishermen regarding aquaculture technology is still low, for example in the selection of broodstock, spawning, hatching, fertilization, larval rearing, nursery, and grow-out.",
+      value: false,
+    },
+    {
+      label:
+        "There is also a lack of fisheries extension workers, so technological innovation is difficult to improve.",
+      value: true,
+    },
+    {
+      label:
+        "Regarding land use, the clash between aquaculture businesses and other development activities (such as settlements, industry, and mining) often harms aquaculture businesses, including the deterioration of water quality.",
+      value: false,
+    },
+    {
+      label:
+        "The highest increase was dominated by marine fishing, which was from 4,383,103 tons in 2003 to 4,647,730 tons in 2007.",
+      value: false,
+    },
+  ],
+};
+
+export default choices;
