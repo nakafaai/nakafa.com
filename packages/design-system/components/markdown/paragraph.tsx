c89825a30792230b271c8579dc3bb9ac2d30d887
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2026-03-04 21:18:46 +0100

    Use tabular-nums for markdown paragraphs

--- packages/design-system/components/markdown/paragraph.tsx
+++ packages/design-system/components/markdown/paragraph.tsx
@@ -5,7 +5,7 @@ export function Paragraph({ children, className, ...props }: ParagraphProps) {
   return (
     <p
       className={cn(
-        "my-4 text-pretty leading-relaxed first:mt-0 last:mb-0",
+        "my-4 text-pretty tabular-nums leading-relaxed first:mt-0 last:mb-0",
         className
       )}
       {...props}
