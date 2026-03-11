0b54c5f7b163dbc9212cd0c70d6d881e774beb34
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2026-03-11 18:53:13 +0100

    Add SNBT Set-2 exercises 21-25

---
+++ packages/contents/exercises/high-school/snbt/indonesian-language/try-out/set-2/21/choices.ts
@@ -0,0 +1,50 @@
+import type { ExercisesChoices } from "@repo/contents/_types/exercises/choices";
+
+const choices: ExercisesChoices = {
+  id: [
+    {
+      label:
+        "Banyak mayat yang ditemukan di sekitar maupun di dalam danau tersebut",
+      value: true,
+    },
+    { label: "Sering kejadian bunuh diri di danau tersebut", value: false },
+    {
+      label: "Zaman dulu disekitar danau tersebut adalah pemukiman",
+      value: false,
+    },
+    {
+      label:
+        "Perperangan terjadi di sekitar danau tersebut sehingga yang meninggal dibiarkan begitu saja",
+      value: false,
+    },
+    {
+      label:
+        "Kutukan alam terhadap orang-orang yang merusak alam sekitar danau tersebut",
+      value: false,
+    },
+  ],
+  en: [
+    {
+      label:
+        "Banyak mayat yang ditemukan di sekitar maupun di dalam danau tersebut",
+      value: true,
+    },
+    { label: "Sering kejadian bunuh diri di danau tersebut", value: false },
+    {
+      label: "Zaman dulu disekitar danau tersebut adalah pemukiman",
+      value: false,
+    },
+    {
+      label:
+        "Perperangan terjadi di sekitar danau tersebut sehingga yang meninggal dibiarkan begitu saja",
+      value: false,
+    },
+    {
+      label:
+        "Kutukan alam terhadap orang-orang yang merusak alam sekitar danau tersebut",
+      value: false,
+    },
+  ],
+};
+
+export default choices;
---
+++ packages/contents/exercises/high-school/snbt/indonesian-language/try-out/set-2/24/choices.ts
@@ -0,0 +1,20 @@
+import type { ExercisesChoices } from "@repo/contents/_types/exercises/choices";
+
+const choices: ExercisesChoices = {
+  id: [
+    { label: "India", value: true },
+    { label: "Yunani", value: false },
+    { label: "Asia Tenggara", value: false },
+    { label: "Rusia", value: false },
+    { label: "Tiongkok", value: false },
+  ],
+  en: [
+    { label: "India", value: true },
+    { label: "Yunani", value: false },
+    { label: "Asia Tenggara", value: false },
+    { label: "Rusia", value: false },
+    { label: "Tiongkok", value: false },
+  ],
+};
+
+export default choices;
