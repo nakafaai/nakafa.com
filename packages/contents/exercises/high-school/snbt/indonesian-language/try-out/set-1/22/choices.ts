760677e29b7b4d061c85d9463a6d56f04a9af981
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2026-03-11 10:00:59 +0100

    Add question 22 and translate Q19-21 prompts

---
+++ packages/contents/exercises/high-school/snbt/indonesian-language/try-out/set-1/22/choices.ts
@@ -0,0 +1,50 @@
+import type { ExercisesChoices } from "@repo/contents/_types/exercises/choices";
+
+const choices: ExercisesChoices = {
+  id: [
+    {
+      label: "Persetujuan dengan jalan damai",
+      value: true,
+    },
+    {
+      label: "Diskusi suatu kelompok",
+      value: false,
+    },
+    {
+      label: "Kerjasama untuk mencapai tujuan bersama",
+      value: false,
+    },
+    {
+      label: "Bermusyawarah dan mufakat dalam mengambil keputusan",
+      value: false,
+    },
+    {
+      label: "Pemungutan suara antar kelompok",
+      value: false,
+    },
+  ],
+  en: [
+    {
+      label: "Persetujuan dengan jalan damai",
+      value: true,
+    },
+    {
+      label: "Diskusi suatu kelompok",
+      value: false,
+    },
+    {
+      label: "Kerjasama untuk mencapai tujuan bersama",
+      value: false,
+    },
+    {
+      label: "Bermusyawarah dan mufakat dalam mengambil keputusan",
+      value: false,
+    },
+    {
+      label: "Pemungutan suara antar kelompok",
+      value: false,
+    },
+  ],
+};
+
+export default choices;
