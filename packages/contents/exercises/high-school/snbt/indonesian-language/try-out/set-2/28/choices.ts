669cde79feeed959bc00a38b6b8e89c351e57d53
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2026-03-11 19:43:48 +0100

    Add try-out set-2 exercises 26–30

---
+++ packages/contents/exercises/high-school/snbt/indonesian-language/try-out/set-2/28/choices.ts
@@ -0,0 +1,20 @@
+import type { ExercisesChoices } from "@repo/contents/_types/exercises/choices";
+
+const choices: ExercisesChoices = {
+  id: [
+    { label: "Asal usul batik Lasem", value: false },
+    { label: "Sejarah pencipta batik Lasem", value: false },
+    { label: "Penemu batik Lasem", value: false },
+    { label: "Motif batik Lasem", value: false },
+    { label: "Jaringan bisnis batik Lasem", value: true },
+  ],
+  en: [
+    { label: "Asal usul batik Lasem", value: false },
+    { label: "Sejarah pencipta batik Lasem", value: false },
+    { label: "Penemu batik Lasem", value: false },
+    { label: "Motif batik Lasem", value: false },
+    { label: "Jaringan bisnis batik Lasem", value: true },
+  ],
+};
+
+export default choices;
