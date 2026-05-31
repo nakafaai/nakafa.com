5f5710fc7ab36dd88bde1f720290cf6ad3bf3a53
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2026-05-31 13:08:56 +0200

    Add NIH SARS-CoV-2 virion 3D asset

---
+++ packages/design-system/components/contents/biology/assets.ts
@@ -0,0 +1,14 @@
+export const SARS_COV_2_VIRION_ASSET = {
+  path: "/models/biology/sars-cov-2-virion-nih3d.glb",
+  sourceTitle: "SARS CoV-2 Virion (NIAID)",
+  sourceUrl: "https://3d.nih.gov/entries/3DPX-013323/2.01",
+  license: "CC-BY-4.0",
+  licenseUrl: "https://creativecommons.org/licenses/by/4.0/",
+  originalFileId: "581060",
+  originalSha256:
+    "f1bff39b83418a83f5fc48bfbbcb693cd10b4d8aab3fe8e9e93af4550f9cef7b",
+  processedSha256:
+    "effb749e69aceb8e53918e3e9d84a27f31b0cfe6100e742188d1405ecab7cc03",
+  processedWith:
+    "gltf-transform weld, then simplify --ratio 0.35 --error 0.002",
+} as const;
