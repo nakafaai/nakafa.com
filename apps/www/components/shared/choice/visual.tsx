f6d1c9e0050d04f5ff9edc4ea941f85d51aa2050
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2026-07-11 17:12:18 +0700

    Smooth choice card color transitions

--- apps/www/components/shared/choice/visual.tsx
+++ apps/www/components/shared/choice/visual.tsx
@@ -14,7 +14,7 @@ export function ChoiceCardVisual({
   return (
     <div className="relative flex aspect-video w-full items-center justify-center">
       <GradientBlock
-        className="mask-[linear-gradient(to_bottom,black_0%,black_65%,transparent_100%)] mask-no-repeat mask-size-[100%_100%] pointer-events-none absolute inset-0"
+        className="mask-[linear-gradient(to_bottom,black_0%,rgba(0,0,0,0.992)_20%,rgba(0,0,0,0.936)_40%,rgba(0,0,0,0.784)_60%,rgba(0,0,0,0.578)_75%,rgba(0,0,0,0.271)_90%,transparent_100%)] mask-no-repeat mask-size-[100%_100%] pointer-events-none absolute inset-0"
         colorScheme="vibrant"
         intensity="medium"
         keyString={seed}
