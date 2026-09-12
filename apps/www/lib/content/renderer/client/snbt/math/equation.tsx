16e0a475ec58fe332891d8e498ea14b5d4ed480f
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2026-09-12 20:51:48 +0200

    fix(content): keep static question graphs in server composition

--- apps/www/lib/content/renderer/client/snbt/math/equation.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-export const LineEquation = dynamic(() =>
-  import(
-    "@repo/design-system/components/contents/mathematics/line/equation"
-  ).then(({ LineEquation: Component }) => Component)
-);
--- apps/www/lib/content/renderer/client/snbt/quant/basics.tsx
+++ apps/www/lib/content/renderer/client/snbt/quant/basics.tsx
@@ -2,12 +2,6 @@
 
 import dynamic from "next/dynamic";
 
-export const LineEquation = dynamic(() =>
-  import(
-    "@repo/design-system/components/contents/mathematics/line/equation"
-  ).then(({ LineEquation: Component }) => Component)
-);
-
 export const NumberLine = dynamic(() =>
   import(
     "@repo/design-system/components/contents/mathematics/number-line"
