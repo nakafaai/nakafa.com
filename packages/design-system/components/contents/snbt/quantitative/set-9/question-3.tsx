23b0dd8f97084564eb0dea07042ab2643cff5294
Nabil Akbarazzima Fatih <nabilakbarazzima@gmail.com>
2026-09-12 22:25:39 +0200

    Move the angle label clear of the coordinate axes

--- packages/design-system/components/contents/snbt/quantitative/set-9/question-3.tsx
+++ packages/design-system/components/contents/snbt/quantitative/set-9/question-3.tsx
@@ -49,7 +49,7 @@ const questionData = [
     showPoints: true,
     labels: [
       { text: <InlineMath math="25^\circ" />, at: 0, offset: [-2, -0.5, 0] },
-      { text: <InlineMath math="\alpha" />, at: 1, offset: [1, 0, 0] },
+      { text: <InlineMath math="\alpha" />, at: 1, offset: [1.45, -0.6, 0] },
       { text: <InlineMath math="135^\circ" />, at: 2, offset: [1, 0.5, 0] },
     ],
   },
